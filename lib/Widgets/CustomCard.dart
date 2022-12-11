bd939af2e1e17aa5e389f995886d9437dd5b55d3
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-12-11 22:34:18 +0530

    icons added

--- lib/Widgets/CustomCard.dart
+++ lib/Widgets/CustomCard.dart
@@ -1,31 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomCard extends StatelessWidget {
   // const CustomCard({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        radius: 25,
-      ),
-      title: Text(
-        '+94771608082',
-        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-      ),
-      subtitle: Row(
+    return InkWell(
+      onTap: () {},
+      child: Column(
         children: [
-          Icon(Icons.done_all),
-          SizedBox(
-            width: 3,
+          ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              child: SvgPicture.asset(
+                "groups.svg",
+                color: Colors.white,
+                height: 36,
+                width: 36,
+              ),
+              backgroundColor: Colors.blueGrey,
+            ),
+            title: Text(
+              '+94771608082',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            subtitle: Row(
+              children: [
+                Icon(Icons.done_all),
+                SizedBox(
+                  width: 3,
+                ),
+                Text(
+                  'Hey',
+                  style: TextStyle(fontSize: 13),
+                ),
+              ],
+            ),
+            trailing: Text("18:00"),
           ),
-          Text(
-            'Hey',
-            style: TextStyle(fontSize: 13),
-          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 20, left: 80),
+            child: Divider(
+              thickness: 1,
+            ),
+          )
         ],
       ),
-      trailing: Text("18:00"),
     );
   }
 }
