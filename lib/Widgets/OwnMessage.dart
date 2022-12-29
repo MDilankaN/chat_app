1d2470c4dfdfc409804405b0c54daa198f03d0c2
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-12-29 21:52:06 +0530

    own message widget

---
+++ lib/Widgets/OwnMessage.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class OwnMessage extends StatelessWidget {
+  const OwnMessage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: ConstrainedBox(
+        constraints:
+            BoxConstraints(maxWidth: MediaQuery.of(context).size.width - 45),
+        child: Card(
+          elevation: 1,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+          color: Color(0xffdcf8c6),
+          child: Stack(
+            children: [
+              // ignore: prefer_const_constructors
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 10, right: 60, top: 10, bottom: 20),
+                // ignore: prefer_const_constructors
+                child: Text(
+                  "Hzxxzxzxzxzxzxzxzxzxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxey",
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ),
+              Positioned(
+                bottom: 4,
+                right: 10,
+                child: Row(
+                  children: [
+                    Text(
+                      "20:59",
+                      style: TextStyle(fontSize: 13, color: Colors.grey[600]),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Icon(
+                      Icons.done_all,
+                      size: 20,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
